039623f2166ee6318478bc073a7631e3f7ce6021
Louis Barnes <Louis@stlbarnes.com>
2018-10-30 11:40:13 -0500

    Javascript1Assignment

---
+++ Javascript1Assignment/Javascript1Assignment.js
@@ -0,0 +1,19 @@
+
+
+var textResponse = ["If you can read this, then great, the program worked!", "If you can read this, then the same thing happened!", "If you can read this, then take a wild guess...", "Hey, can you read this? Great!", "This is filler to reach the Assignment requirements", "This is also filler and the sixth element"];
+
+var el = document.getElementById("first");
+
+el.textContent = textResponse[1];
+
+var el = document.getElementById("second");
+
+el.textContent = textResponse[2];
+
+var el = document.getElementById("third");
+
+el.textContent = textResponse[3];
+
+var el = document.getElementById("fourth");
+
+el.textContent = textResponse[4];
